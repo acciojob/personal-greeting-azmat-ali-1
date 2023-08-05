25039d86fa3e2ef308fe7760d142f9d1932f8dad
AZMAT ALI <222azmat@gmail.com>
2023-08-05 19:01:03 +0530

    submitting code-Sat Aug 05 2023 19:01:01 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,14 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let i = document.getElementById("input");
+  let p = document.getElementById("para");
+  p.innerText = i.value;
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Enter your name:</p>
+        <input type="text" id= "input"></input>
+        <p id="para"></p>
     </div>
   )
 }
